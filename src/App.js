ec12c901b204f4d4560c101d429c85464e2476ca
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2021-01-06 16:21:15 +0900

    Creating React component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-//props, { props.fav } === ({ fav }). { fav } 
-function Potato({ fav }) {
-  return (
-    <h2>I love { fav }! very much.</h2>
-  )
-}
-
-function App() {
-  return (
+class App extends React.Component {
+  //바꿀 데이터를 state 안에 넣는다
+  state = { //state is an object
+    count: 0
+  };
+  plus = () => {
+    //state를 새로 만들어준다, setState를 사용하지 않으면 새 state와 함께 render()를 호출하지 않는다 
+    this.setState((current) => ({count: current.count + 1})) //setState(((current) => ({count: count.current.count +1})))
+  };
+  minus = () => {
+    this.setState((current) => ({count: current.count - 1}))
+  };
+  render() {
+    return (
     <div>
-      <h2>Hello React Js, this is Clem here!</h2>
-      {/** fav="Potato" :: props */}
-      <Potato fav="Potato" /> {/**adding component in App.js */} 
-      <Potato fav="Ramen" />
-      <Potato fav="Kimbab" />
-      <Potato fav="Chicken" />
+      <h2>The class number is : { this.state.count }</h2>
+      <button onClick={this.plus}>Plus</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
