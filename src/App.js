35b54254119ce43b1000036c533069b8d595b305
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2021-01-06 11:24:01 +0900

    Create reusable component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import Potato from './Potato'
+
+//props, { props.fav } === ({ fav }). { fav } 
+function Potato({ fav }) {
+  return (
+    <h2>I love { fav }! very much.</h2>
+  )
+}
 
 function App() {
   return (
     <div>
       <h2>Hello React Js, this is Clem here!</h2>
-      <p>I am here to learn React JS!</p>
-      <Potato /> {/**adding component in App.js */}
+      {/** fav="Potato" :: props */}
+      <Potato fav="Potato" /> {/**adding component in App.js */} 
+      <Potato fav="Ramen" />
+      <Potato fav="Kimbab" />
+      <Potato fav="Chicken" />
     </div>
   );
 }
