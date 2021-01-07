fbb05dc5dda48d8f603b351aa72a06ce60d3fab5
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2021-01-07 13:03:52 +0900

    Creating css

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) { //파라미터 : 객체로 가져와야한다.
+function Movie({ id, year, title, summary, poster, genres }) { //파라미터 : 객체로 가져와야한다.
   return (
-    <div class="movies">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div class="movie__data"> 
-        <h3 class="movie__title">{title}</h3>
-        <h5 class="movie__year">{year}</h5>
-        <p class="movie__summary">{summary}</p>
+      <div className="movie__data"> 
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <p className="movie__summary">{summary.slice(0, 140)}...</p> {/** summary를 0~140까지만 출력 후 ...로 처리 */}
+        <ul className="genres">{genres.map((genre, index) => <li key={index}className="genres__genre">{genre}</li>)}</ul>
       </div>
     </div>
   )
@@ -20,7 +21,8 @@ Movie.prototypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
